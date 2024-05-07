5ff44e19d60440aa45d0d3656df6b04ec18433df
Berry6753 <163379552+Berry6753@users.noreply.github.com>
2024-05-07 10:40:21 +0900

    asdf
    asdf

---
+++ Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+public class PlayerUI : MonoBehaviour
+{
+    [SerializeField]
+    private Slider hpbar;
+
+    [SerializeField]
+    public int maxkillcount=5;
+
+    public int currentkillcount;
+
+    public TextMeshProUGUI killcount;    
+    private float MaxHp;
+    private float currentHp;
+    public GameObject player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHp = player.GetComponent<PlayerController>().BasePlayerHp;
+        MaxHp = player.GetComponent<PlayerController>().BasePlayerHp;
+        hpbar.value = currentHp / MaxHp;
+        currentkillcount = 0;
+    }
+
+   
+    void Update()
+    {
+        killcount.text = $" {currentkillcount} / {maxkillcount}";
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            GetDamaged(10);
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            currentkillcount++;
+            Debug.Log($"Ä«¿îÆ® :  {currentkillcount}");
+        }
+
+        if(currentHp <= 0)
+        {
+            SceneManager.LoadScene("DeadScene");
+        }
+
+        if(currentkillcount >= maxkillcount)
+        {
+            SceneManager.LoadScene("ClearScene");
+        }
+    }
+
+    public void GetDamaged(float num)
+    {
+        currentHp -= num;
+
+        CurrentHp();
+    }
+    public void CurrentHp()
+    {
+        hpbar.value = currentHp / MaxHp;
+    }
+    
+}
